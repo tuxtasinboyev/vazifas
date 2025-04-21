8562cb1c197d201643a872cb3cc8c2b750146524
Adam75757 <asqaraliyevfaxriddin2011@gmail.com>
2025-04-21 18:46:12 +0500

    biror

--- controller/index.js
+++ controller/index.js
@@ -1,10 +1,30 @@
-const GET=()=>{
+import fs from "fs"
+import path from "path"
 
-}
+let data = JSON.parse(fs.readFileSync(path.join(process.cwd(), "/vazifas/data/users.json"), "utf-8"));
+
+const GET = (req, res) => {
+   
+   res.send(data);
+};
+
 const GET_ID=()=>{
 
 }
-const POST=()=>{
+const POST=(req,res)=>{
+
+   let index = data.findIndex(l => l.id === req.body.id)
+
+        if(index!=-1){
+            res.end("Xato bu id bor")
+        } else{
+
+        data.push(req.body)
+
+        fs.writeFileSync("vazifas/data/users.json",JSON.stringify(data,null,4))
+
+        res.send("yozildi")
+        }
 
 }
 const  DELETE=()=>{
