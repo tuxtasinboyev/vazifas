673f8cf07b520ccbe4a7bc98fb839c62821e38d5
Omadbek <shustrbola@gmail.com>
2025-04-21 18:34:01 +0500

    get id buldi

--- controller/index.js
+++ controller/index.js
@@ -1,18 +1,31 @@
-const GET=()=>{
+import fs from "fs"
+import path from "path"
+const GET = () => {
 
 }
-const GET_ID=()=>{
+
+const GET_ID = (req, res) => {
+    const { id } = req.params;
+
+    const users = JSON.parse(fs.readFileSync(path.join(process.cwd(), "/data/users.json"), "uatf-8"));
+
+    const user = users.find(user => user.id === Number(id));
+
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({ error: "User not found" });
+    }
+};
+const POST = () => {
 
 }
-const POST=()=>{
+const DELETE = () => {
 
 }
-const  DELETE=()=>{
-
-}
- export default{
+export default {
     GET,
     GET_ID,
     POST,
     DELETE
- }
+}
